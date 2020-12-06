010a09b77ba1c2fb0353cfca52718ef029cbde84
Jim Barrows <Jim.Barrows@gmail.com>
2020-12-05 18:39:39 -0600

    Chart of accounts for internal organziations is done.

--- sql/02-create-database.sql
+++ sql/02-create-database.sql
@@ -22,21 +22,22 @@ create table if not exists general_ledger_account
     name        text not null
         CONSTRAINT general_ledger_account_type_description_not_empty CHECK (name <> ''),
     description text,
+    type_id     uuid not null references general_ledger_account_type (id),
     CONSTRAINT general_ledger_account_pk PRIMARY key (id)
 );
 
 create table if not exists organization_gl_account
 (
-    id                           uuid          DEFAULT uuid_generate_v4(),
-    from_date                    date not null default current_date,
-    thru_date                    date,
-    for_general_ledger_account   uuid not null references general_ledger_account (id),
-    for_internal_organization    uuid not null,
-    subsidiary_account_of        uuid references organization_gl_account (id),
-    referencing_product          uuid,
-    referencing_product_category uuid,
-    referencing_bill_to_customer uuid,
-    referencing_supplier         uuid,
+    id                              uuid          DEFAULT uuid_generate_v4(),
+    from_date                       date not null default current_date,
+    thru_date                       date,
+    general_ledger_account_id       uuid not null references general_ledger_account (id),
+    internal_organization_id        uuid not null,
+    subsidiary_account_of_id        uuid references organization_gl_account (id),
+    referencing_product_id          uuid,
+    referencing_product_category_id uuid,
+    referencing_bill_to_customer_id uuid,
+    referencing_supplier_id         uuid,
     CONSTRAINT organization_gl_account_pk PRIMARY key (id)
 );
 
@@ -46,7 +47,8 @@ create table if not exists accounting_period
     accounting_period_number bigint not null default 1,
     from_date                date   not null default current_date,
     thru_date                date,
-    within_accounting_period uuid references accounting_period (id),
+    parent_id                uuid references accounting_period (id),
+    period_type_id           uuid references period_type (id),
     CONSTRAINT accounting_period_pk PRIMARY key (id)
 );
 
